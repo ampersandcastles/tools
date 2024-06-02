c0f300cc329c32245792e7d1437008e9a886ff60
Tristan Smith <tristan.smith@gmail.com>
2024-06-01 20:29:42 -0400

    no

--- packet.py
+++ packet.py
@@ -1,13 +1,10 @@
 from scapy.all import send, IP, UDP
 
-# Define the destination IP address and UDP ports
-destination_ip = '255.255.255.255'  # Destination IP address
-source_ip = '192.168.1.111'         # Source IP address (change as needed)
-source_port = 14236                 # Source port
-destination_port = 14235            # Destination port
+destination_ip = '255.255.255.255'
+source_ip = '192.168.1.121'
+source_port = 14236
+destination_port = 14235
 
-# Construct the packet
 packet = IP(src=source_ip, dst=destination_ip) / UDP(sport=source_port, dport=destination_port)
 
-# Send the packet
 send(packet)
