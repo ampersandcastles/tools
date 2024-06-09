ab37c20ba5b0bf5640b61b5990a2c955cef09503
Tristan <tristan.smith@gmail.com>
2024-06-09 16:22:42 -0400

    Update packet.py
    typo

--- packet.py
+++ packet.py
@@ -1,6 +1,5 @@
 from scapy.all import sendp, Ether, IP, UDP
 
-# Define the parameters
 destination_ip = '255.255.255.255'
 source_ip = '192.168.1.121'
 source_mac = '8c:16:45:68:54:80'
@@ -8,8 +7,6 @@ destination_mac = 'ff:ff:ff:ff:ff:ff'  # Broadcast MAC address
 source_port = 14236
 destination_port = 14235
 
-# Construct the packet with Ethernet layer
 packet = Ether(src=source_mac, dst=destination_mac) / IP(src=source_ip, dst=destination_ip) / UDP(sport=source_port, dport=destination_port)
 
-# Send the packet at layer 2 (Ethernet layer)
 sendp(packet)
