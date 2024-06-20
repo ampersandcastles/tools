b59eee228a50d5e8afe124307f8d5168f530561c
Tristan Smith <tristan.smith@gmail.com>
2024-06-20 01:27:17 -0400

    fuckin' with a packet

--- packet.py
+++ packet.py
@@ -1,9 +1,9 @@
 from scapy.all import sendp, Ether, IP, UDP
 
 destination_ip = '255.255.255.255'
-source_ip = '192.168.100.223'
-source_mac = '8c:16:45:68:54:80'
-destination_mac = 'ff:ff:ff:ff:ff:ff'  # Broadcast MAC address
+source_ip = '192.168.1.1'
+source_mac = '52:54:00:ac:05:8c'
+destination_mac = 'ff:ff:ff:ff:ff:ff'
 source_port = 14236
 destination_port = 14235
 
